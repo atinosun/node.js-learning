d6f51c28d55a2ee21fe4a3af2ffeaa31b60cc380
atinosun <ationsun@yeah.net>
2017-04-06 11:08:08 +0800

    添加入口文件。将整个公司模块的方法暴露出去。

--- startProject/02-company/company.js
+++ startProject/02-company/company.js
@@ -3,10 +3,21 @@
  * User: atinosun
  * Date: 2017/2/22
  * Time: 23:22
+ * Updated_date:2017/4/06
+ * Time:11:01
  */
 //声明局部变量
 var employees = require("./employees");
 var leaders = require("./leaders");
 
-employees.add("employee1");
-leaders.add("leader1");
+/*employees.add("employee1");
+ leaders.add("leader1");*/
+
+function add(leaderName, employeesNames) {
+    leaders.add(leaderName);
+    employeesNames.forEach(function (item, index) {
+        employees.add(item);
+    })
+}
+
+exports.add = add;
---
+++ startProject/02-company/index.js
@@ -0,0 +1,9 @@
+/**
+ * Description:入口文件
+ * User: atinosun
+ * Date: 2017/4/06
+ * Time: 11:02
+ */
+var company = require('./company');
+
+company.add('领导', ['雇员１', '雇员２']);
