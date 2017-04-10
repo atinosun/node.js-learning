d2ce31cee4fc7558533de4b9861cbef6508bf72e
atinosun <ationsun@yeah.net>
2017-04-10 15:21:49 +0800

    添加url.resolve方法。

--- startProject/03-url/url_resolve.js
+++ startProject/03-url/url_resolve.js
@@ -4,3 +4,8 @@
  * Date: 2017/4/06
  * Time: 13:59
  */
+const url = require('url');
+var sourceUrl = 'http://user:pass@host.com:8080/p/a/t/h?query=string#hash';
+var replaceUrl = '/one/two';
+console.log(url.resolve(sourceUrl, replaceUrl));
+
